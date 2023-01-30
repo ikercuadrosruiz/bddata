bf12fc9cb18a4035cd178c5f68a37919946bf633
ikercuadrosruiz <122614510+ikercuadrosruiz@users.noreply.github.com>
2023-01-30 10:02:52 +0100

    datos
    fd

---
+++ datosMovimientos.sql
@@ -0,0 +1,24 @@
+INSERT INTO `movimientos` (`id`,`tipo`,`fechaoperacion`,`descripcion`,`importe`,`idcuenta`)
+VALUES
+  (1,"D","2022-05-23 08:19:35","nascetur ridiculus mus. Proin vel arcu eu odio tristique pharetra. Quisque ac libero nec",94,13),
+  (2,"O","2023-08-07 16:00:45","sit amet, risus. Donec nibh enim, gravida sit amet, dapibus id, blandit at, nisi. Cum",77,9),
+  (3,"O","2022-03-10 15:14:28","rutrum urna,",55,9),
+  (4,"O","2022-12-15 18:55:04","est.",59,4),
+  (5,"O","2022-09-15 23:36:03","nec",46,9),
+  (6,"D","2022-11-06 21:16:40","pulvinar arcu et pede. Nunc sed orci lobortis augue",7,12),
+  (7,"D","2022-10-15 19:31:35","aptent taciti sociosqu ad litora torquent per",50,7),
+  (8,"O","2023-11-14 17:15:38","Cras dictum ultricies ligula. Nullam enim. Sed nulla ante, iaculis nec, eleifend non, dapibus rutrum, justo. Praesent luctus. Curabitur",64,9),
+  (9,"D","2023-11-21 09:43:36","Sed diam lorem, auctor quis, tristique ac, eleifend vitae, erat. Vivamus nisi. Mauris nulla. Integer urna. Vivamus molestie dapibus",59,16),
+  (10,"D","2022-04-28 23:28:09","quam quis diam. Pellentesque habitant morbi tristique senectus et netus et malesuada",86,12);
+INSERT INTO `movimientos` (`id`,`tipo`,`fechaoperacion`,`descripcion`,`importe`,`idcuenta`)
+VALUES
+  (11,"D","2022-08-22 05:58:50","in felis. Nulla tempor",33,9),
+  (12,"O","2022-10-21 11:38:23","mollis vitae, posuere at, velit. Cras lorem lorem, luctus ut, pellentesque eget, dictum placerat, augue.",74,7),
+  (13,"D","2022-12-02 09:48:48","leo. Morbi neque tellus, imperdiet non, vestibulum",84,4),
+  (14,"D","2022-05-18 19:33:48","ac nulla. In tincidunt congue turpis. In condimentum. Donec at arcu. Vestibulum ante ipsum primis",51,5),
+  (15,"D","2023-10-13 13:51:09","imperdiet nec, leo. Morbi neque tellus, imperdiet non, vestibulum",85,9),
+  (16,"O","2022-05-20 23:00:26","Suspendisse non leo. Vivamus nibh dolor, nonummy ac, feugiat non, lobortis quis, pede. Suspendisse dui. Fusce diam nunc,",39,18),
+  (17,"O","2022-07-17 07:40:51","diam dictum sapien. Aenean massa. Integer vitae nibh.",74,1),
+  (18,"D","2023-06-10 01:50:59","mus. Donec dignissim magna a tortor. Nunc commodo",66,6),
+  (19,"D","2022-03-26 16:21:43","ac mattis semper, dui lectus rutrum urna, nec luctus felis purus",49,1),
+  (20,"D","2022-04-23 06:21:53","velit. Quisque varius. Nam porttitor scelerisque neque.",13,17);
